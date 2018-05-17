9a79717bed18c68b1097863c858895ca5bae388b
ajshiff <ajshiffler@gmail.com>
2018-05-17 12:28:25 -0600

    Proposed Solution to Exercise 4. Needs to be Tested Though.

--- learnyounode-e04.js
+++ learnyounode-e04.js
@@ -1,3 +1,30 @@
+//https://github.com/maxogden/art-of-node#callbacks
+//https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 const fs = require('fs');
 cargs = process.argv;
-//https://github.com/maxogden/art-of-node#callbacks).
+var number = 0;
+
+/*************************************************************************
+ * Calls the readFile function. It is async, so node will continue running
+ * the rest of the page whilst this function is attending to it's own 
+ * internal components.
+ *************************************************************************/
+function readFile(callback){
+    fs.readFile(cargs[2], 'utf8', function doneReading(err, fileContents){
+        if (err) {throw err;}
+        callback(fileContents);
+    })
+}
+
+/*************************************************************************
+ * This is the callback function that readFile() will take and run when 
+ * readFile finishes reading the file.
+ *************************************************************************/
+function countNewLines(fileContent) {
+    //console.log(fileContent);
+    fileContent = fileContent.split("\n");
+    console.log(fileContent.length-1);
+}
+
+//Main Output
+readFile(countNewLines);
