ee6278fce5c039145fea8deb4065b214ab9e9b7a
ajshiff <ajshiffler@gmail.com>
2018-05-18 13:05:26 -0600

    Completed e05, and e06a/e06b. Small quirk in passing e06 test, but it's fine.

--- learnyounode-e05.js
+++ learnyounode-e05.js
@@ -1,1 +1,23 @@
-console.log("HELLO WORLD");
+//file:///C:/Users/ajshiff/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/path.html#path_path_extname_p
+//file:///C:/Users/ajshiff/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/fs.html
+const fs = require('fs');
+const path = require('path');
+fs.readdir(process.argv[2], function(err, list){
+    list = list.reduce((acc, item) => {if(path.extname(item) === '.'+process.argv[3]){acc.push(item);} return acc;}, []);
+    list.forEach(item => console.log(item));
+});
+/* 
+//Provided solution//
+var fs = require('fs');
+var path = require('path');
+var folder = process.argv[2];
+var ext = '.' + process.argv[3];
+fs.readdir(folder, function (err, files) {
+    if (err) return console.error(err);
+    files.forEach(function (file) {
+        if (path.extname(file) === ext) {
+             console.log(file);
+        }
+    });
+});
+*/
---
+++ learnyounode-e06b.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+/*
+module.exports =  
+{
+    byExtension: function (directory, extension, callbackFunction){
+        fs.readdir(directory, function(error, data){
+            if (error)
+                callbackFunction(error);
+            else
+                callbackFunction(error, data);
+        });
+    }
+};
+*/
+module.exports = function (directory, extension, callbackFunction){
+    fs.readdir(directory, 'utf8', function(error, data){
+        if (error) 
+            return callbackFunction(error);
+        else
+            callbackFunction(error, data);
+        //console.log(data);
+        //return data;
+        //return data.length;
+    });
+}
