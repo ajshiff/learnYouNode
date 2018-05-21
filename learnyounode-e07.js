a3bd5bb3ccde7324ba3a9c4167b3c676b8041fe0
ajshiff <ajshiffler@gmail.com>
2018-05-21 12:34:28 -0600

    Completed learnyounode challenge 7: http requests

--- learnyounode-e07.js
+++ learnyounode-e07.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD");
+const http = require('http');
+const url = process.argv[2];
+http.get(url, function (response) {
+    response.setEncoding('utf8').on('data', function(data){console.log(data);});;
+    //response.on('data', function(data){console.log(data);});
+    //console.log(response.statusCode);
+    //console.log(response);
+});
