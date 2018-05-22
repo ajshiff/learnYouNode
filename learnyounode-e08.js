b660f4a5423d8c1e85ba48cd9235ac7e92b29b02
ajshiff <ajshiffler@gmail.com>
2018-05-22 14:00:59 -0600

    Started lyn-e08 challenge

--- learnyounode-e08.js
+++ learnyounode-e08.js
@@ -1,1 +1,11 @@
-console.log("HELLO WORLD");
+const http = require('http');
+const url = process.argv[2];
+var stringToChar = [];
+
+http.get(url, function(response){
+    response.setEncoding('utf8').on('data', function(data){
+        stringToChar = data.split("");
+        //stringToChar = stringToChar.reduce((acc, ele) => acc.concat(ele), []);
+        console.log(stringToChar);
+    });
+});
