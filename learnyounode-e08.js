c55be8790695fec3cdbc03ce87caffd597c02911
ajshiff <ajshiffler@gmail.com>
2018-05-23 13:11:34 -0600

    Machine wants me to commit for some reason

--- learnyounode-e08.js
+++ learnyounode-e08.js
@@ -3,9 +3,9 @@ const url = process.argv[2];
 var stringToChar = [];
 
 http.get(url, function(response){
-    response.setEncoding('utf8').on('data', function(data){
-        stringToChar = data.split("");
+    //response.setEncoding('utf8').on('data', function(data){
+        stringToChar = response.split("");
         //stringToChar = stringToChar.reduce((acc, ele) => acc.concat(ele), []);
         console.log(stringToChar);
-    });
+    //});
 });
