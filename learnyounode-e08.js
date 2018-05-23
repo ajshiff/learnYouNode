75b30f564abfd3231ca581c05ff298341206c162
ajshiff <ajshiffler@gmail.com>
2018-05-23 12:57:47 -0600

    add response.on(end) functionality

--- learnyounode-e08.js
+++ learnyounode-e08.js
@@ -1,11 +1,16 @@
 const http = require('http');
 const url = process.argv[2];
 var stringToChar = [];
-
+var dataString = "";
+var isCollectingData;
 http.get(url, function(response){
     response.setEncoding('utf8').on('data', function(data){
-        stringToChar = data.split("");
+        //stringToChar = data.split("");
         //stringToChar = stringToChar.reduce((acc, ele) => acc.concat(ele), []);
-        console.log(stringToChar);
+        dataString += data;
+        //console.log(dataString);
     });
+    response.on('end',function(){
+        console.log("end of stream");
+    })
 });
