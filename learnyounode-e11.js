b3ac2486ea459828c412f5bf5431466cd3ad80d4
Shiffler <ajshiff@byui.edu>
2018-05-25 13:09:05 -0600

    Add Base Structure to HTTP Server

--- learnyounode-e11.js
+++ learnyounode-e11.js
@@ -3,3 +3,9 @@ const http = require('http');
 const port = process.argv[2];
 const fileLocation = process.argv[3];
 
+http.createServer(function (request, response) {
+    console.log(request);
+    console.log('\n');
+    console.log(response);
+    response.end();
+}).listen(port);
