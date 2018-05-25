8eb9f3e4e344682217124331b842e458a3745789
Shiffler <ajshiff@byui.edu>
2018-05-25 13:43:32 -0600

    Conforms to eslint standard

--- learnyounode-e08.js
+++ learnyounode-e08.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const url = process.argv[2];
 var stringToChar = [];
-var dataString = "";
-var isCollectingData;
-http.get(url, function(response){
-    response.setEncoding('utf8').on('data', function(data) {dataString += data;});
-    response.on('end', function(){
-        stringToChar = dataString.split("");
+var dataString = '';
+
+http.get(url, function (response) {
+    response.setEncoding('utf8').on('data', function (data) {
+        dataString += data;
+    });
+    response.on('end', function () {
+        stringToChar = dataString.split('');
         console.log(stringToChar.length);
         console.log(dataString);
-    })
+    });
 });
