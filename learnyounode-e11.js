bd91943e9d39be2e5bfce6a6ddc028acb8c8becb
Shiffler <ajshiff@byui.edu>
2018-05-25 17:09:40 -0600

    Remove Excess Code

--- learnyounode-e11.js
+++ learnyounode-e11.js
@@ -2,16 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const port = process.argv[2];
 const fileLocation = process.argv[3];
-let stream = fs.createReadStream(fileLocation);
-//console.log(stream);
+var stream = fs.createReadStream(fileLocation);
+
 http.createServer(function (request, response) {
-    //console.log(request);
-    //console.log('\n');
-    //console.log(response);
-    //response.write(stream)
-    //response.write('Hello Internet');
     stream.pipe(response);
-    //fs.createReadStream(fileLocation).pipe(response);
-    //fs.createReadStream((process.argv[3]).pipe(response));
     //response.end();
 }).listen(port);
