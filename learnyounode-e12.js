e20fec0b1e4364a137c7e9daac0077d1894a7fb6
Shiffler <ajshiff@byui.edu>
2018-05-25 17:45:01 -0600

    Receive Request, convert to string and uppercase. NEEDS: send change back to user >> response.

--- learnyounode-e12.js
+++ learnyounode-e12.js
@@ -1,1 +1,15 @@
-console.log("HELLO WORLD");
+const http = require('http');
+const port = process.argv[2];
+
+http.createServer(function (request, response) {
+    let dataCollector = '';
+    request.setEncoding('utf8').on('data', function (data) {
+        data += '';
+        dataCollector += data;
+    });
+    request.on('end', function () {
+        dataCollector = dataCollector.toUpperCase();
+        console.log(dataCollector);
+    });
+
+}).listen(port);
