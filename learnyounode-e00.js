e76882e25767f551013d02347864db78e324e646
ajshiff <ajshiffler@gmail.com>
2018-05-29 17:52:33 -0600

    Started testing some solutions, but not found one yet

--- learnyounode-e00.js
+++ learnyounode-e00.js
@@ -1,9 +1,6 @@
-var http = require('http');
-var port = process.argv[2];
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    response.write(request.url);
-    response.end();
-}).listen(port);
+const http = require('http');
+const fs = require('fs');
+const text = process.argv[2];
+
+var idk = fs.createReadStream(text);
+console.log(idk);
--- learnyounode-e12.js
+++ learnyounode-e12.js
@@ -1,15 +1,19 @@
 const http = require('http');
+//const map = require('through2-map');
 const port = process.argv[2];
-
 http.createServer(function (request, response) {
-    let dataCollector = '';
-    request.setEncoding('utf8').on('data', function (data) {
-        data += '';
-        dataCollector += data;
-    });
-    request.on('end', function () {
-        dataCollector = dataCollector.toUpperCase();
-        console.log(dataCollector);
-    });
+    if (request.method === 'POST'){
+        let dataCollector = '';
+        request.setEncoding('utf8').on('data', function (data) {
+            data += '';
+            dataCollector += data;
+        })
+        request.on('end', function () {
+            dataCollector = dataCollector.toUpperCase();
+            //console.log(dataCollector);
+            //dataCollector.pipe(response);
+        }).pipe().pipe(response);
+    }
+    
 
 }).listen(port);
